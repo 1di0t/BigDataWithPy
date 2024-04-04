46e245da29be694954646dca41ed61d76c6ad600
1di0t <jeonjerry0709@gmail.com>
2024-04-04 13:59:55 +0900

    join and strip example

---
+++ week05_textStrings01.py
@@ -0,0 +1,8 @@
+subject = ['BigData','IOT','NETWORK                  ']
+print(subject[2].lstrip())
+
+total = " | ".join(subject)
+print(total)
+sentance = "pikachu used ThunderBolt??!"
+print(sentance.strip('!?'))
+print(sentance.replace('ThunderBolt','BlackOut'))
