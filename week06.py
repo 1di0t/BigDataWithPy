29652d2b24799ff730e2b85c0738ef0db4c0a590
1di0t <jeonjerry0709@gmail.com>
2024-04-11 12:06:35 +0900

    prototype kiosk with while

--- week06.py
+++ week06.py
@@ -1,10 +1,17 @@
-texts = input("input sentence : ")
-if texts.isalnum():
-    print(f"only alphabet and numeric!!")
-else:
-    print(f"there are another thing is in sentence besides alphabet and numeric")
-# try:
-#     if texts.index(finding_text) >= 0:
-#         print(texts.index(finding_text))
-# except ValueError:
-#     print(f"입력하신 문장에서 {finding_text} 를 찾을 수 없습니다")
+beverage = ['Espresso','Compagina']
+prices = [2000,2500]
+total_Price = 0
+
+while True:
+    menu_num = int(input(f" {0}) {beverage[0]} : {prices[0]} \n {1}) {beverage[1]} : {prices[1]} \n {len(beverage)+1}) exit\n 메뉴를 입력하세요 :"))
+    if menu_num == 3:
+        print("주문확정")
+        break
+    elif menu_num == 1:
+        print(f"{beverage[0]} 주문완료")
+        total_Price += prices[0]
+    elif menu_num == 2:
+        print(f"{beverage[1]} 주문완료")
+        total_Price += prices[1]
+
+print(f"최종 금액 {total_Price}")
