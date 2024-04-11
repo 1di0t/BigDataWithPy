bb784adafa509b2ea916c7d8aa66ef2e28223604
1di0t <jeonjerry0709@gmail.com>
2024-04-11 11:25:53 +0900

    find,rfind and index with try/except

--- week06.py
+++ week06.py
@@ -1,7 +1,15 @@
-texts = "Pickachu ThunderBolt"
+texts = input("input sentence : ")
+finding_text = input("input finding text : ")
+if texts.find(finding_text) == -1:
+    print(f"입력하신 문장에서 {finding_text} 를 찾을 수 없습니다")
+try:
+    if texts.index(finding_text) >= 0:
+        print(texts.index(finding_text))
+except ValueError:
+    print(f"입력하신 문장에서 {finding_text} 를 찾을 수 없습니다")
+#texts = "pikachu used ThunderBolt and died??!pikachu"
 
-print(texts[9::2])
-texts = texts.replace("ThunderBolt","Spark")#convert ThunderBolt to Spark
-texts = "H"+texts[1:]
-texts = texts.replace("c"," ",2)#convert 'c' to ' ' twice
-print(texts)
+# print(texts.find("pikachu"))#start at 0
+# print(texts.rfind("pikachu"))#start at end of texts
+# print(texts.rfind("daelim"))#return -1
+# print(texts.index("daelim"))#Value Error
