d2fcfe2fe318f9816349340fdc44e3a65bf07d18
1di0t <jeonjerry0709@gmail.com>
2024-04-11 10:46:15 +0900

    replace function with explain

--- week06.py
+++ week06.py
@@ -1,7 +1,7 @@
 texts = "Pickachu ThunderBolt"
 
 print(texts[9::2])
-texts = texts.replace("ThunderBolt","Spark")
+texts = texts.replace("ThunderBolt","Spark")#convert ThunderBolt to Spark
 texts = "H"+texts[1:]
-texts = texts.replace("c"," ",2)
+texts = texts.replace("c"," ",2)#convert 'c' to ' ' twice
 print(texts)
