a8fe8116f5ffc1a74f9173fab504040b8a05ace2
1di0t <jeonjerry0709@gmail.com>
2024-05-02 11:09:10 +0900

    keyosk print menu with dictionary

--- week08.py
+++ week08.py
@@ -1,18 +1,23 @@
-shu = {"Name":"현엉","Gender":"woman","Height":405,"Weight":1450,"Vision":[55.0,73.0]}
+# beverage = ['Espresso', 'Compagina', 'Lemonade']
+# prices = [2000, 2500, 3900]
 
-for i in shu:#keys
-    print(i,end=' ')
-print()
-for i in shu.values():#values
-    print(i,end=' ')
-print()
-for i in shu.items():#key & values
-    print(i,end=' ')
-print()
-for i in shu.items():#key & values
-    print(f'{i[0]} is  {i[1]}')
-print()
-for i,k in shu.items():#unpacking
-# #insert each values in another val
-    print(f'{i} is  {k}')
-print()
+beverage_Price = {'Espresso':2000,'Compagina':2500,'Lemonade':3900}
+total_Price = 0
+quantity = [0, 0, 0]
+
+
+def print_menu():
+    i = 0
+    for menu, price in beverage_Price.items():
+        print(f"{i + 1}) {menu:<15}  {price:>6}")
+        i= i+1
+    print(f"{len(beverage_Price)+1}) exit")
+
+while True:
+    print_menu()
+    try:
+        menu_num = int(input("메뉴를 입력하세요 : "))
+    except:
+        print("숫자를 입력해주세요")
+        continue
+
