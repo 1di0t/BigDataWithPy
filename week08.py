86ff7de148c420eee351dbda86ab2cfbaf0800df
1di0t <jeonjerry0709@gmail.com>
2024-05-02 10:27:36 +0900

    dictionory and how to get values

--- week08.py
+++ week08.py
@@ -1,5 +1,18 @@
 shu = {"Name":"현엉","Gender":"woman","Height":405,"Weight":1450,"Vision":[55.0,73.0]}
-print(type(shu))
-print(type(shu['Vision']))
-shu['Vision'][0]=12.0
-print(shu['Vision'][0])
+
+for i in shu:#keys
+    print(i,end=' ')
+print()
+for i in shu.values():#values
+    print(i,end=' ')
+print()
+for i in shu.items():#key & values
+    print(i,end=' ')
+print()
+for i in shu.items():#key & values
+    print(f'{i[0]} is  {i[1]}')
+print()
+for i,k in shu.items():#unpacking
+# #insert each values in another val
+    print(f'{i} is  {k}')
+print()
