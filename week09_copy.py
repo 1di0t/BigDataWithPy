61eb99353078acd4de2f83dd6b1fd9cadfab7f5a
1di0t <jeonjerry0709@gmail.com>
2024-05-09 11:30:19 +0900

    shallow copy v2

--- week09_copy.py
+++ week09_copy.py
@@ -1,5 +1,8 @@
-a= [-9,11,7]
-b=a
-print(a,b)
-b[0] =100
-print(a,b)
+a= [[-9,8],11,7]
+#shallow copy
+b=a.copy()#copy list a data in list b
+c= list(a)
+d=a[:]
+print(a,b,c,d)
+b[0][1] =100
+print(a,b,c,d)
