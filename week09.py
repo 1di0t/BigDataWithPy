4942fd2f9d9bb285584d3a6bf35b77efd2b36976
1di0t <jeonjerry0709@gmail.com>
2024-05-09 10:32:07 +0900

    tutple and List

---
+++ week09.py
@@ -0,0 +1,10 @@
+# beverage_Price = {
+#     'Espresso':2000,
+#     'Compagina':2500,
+#     'Lemonade':3900,
+#     'Plain Yogurt':4500}
+
+beverage_price_quantity = (['Espresso',2000,0],['Compania',2000,0],['Lemonade',2000,0],['Plain Yogurt',2000,0])
+print(type(beverage_price_quantity))
+beverage_price_quantity = dict(beverage_price_quantity)
+print(type(beverage_price_quantity))
