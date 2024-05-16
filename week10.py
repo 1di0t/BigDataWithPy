bcfe32b3efd4bdea888ce294904eb49684f771f3
1di0t <jeonjerry0709@gmail.com>
2024-05-16 11:17:37 +0900

    version.0.1.2 added odd or even

--- week10.py
+++ week10.py
@@ -16,25 +16,30 @@ def is_prime(k) -> bool:
             i = i + 1
         return True
 
-def prime():
+def primeAndOddEven():
     number =en_input_number.get()
     text =""
-    if is_prime(int(number)): text = "소수"
+    iseven =""
+    number =int(number)
+    if is_prime(number): text = "소수"
     else : text = "소수가 아님"
-    lab_print_prime_num.config(text=f"{number}는 {text}")
+    if number%2==0: iseven = "짝수"
+    else : iseven = "홀수"
+    lab_print_prime_and_odd_even_num.config(text=f"{number}는 {text}\n{number}는 {iseven}")
+
 
 window = tk.Tk()
 window.geometry("500x200")#Size
 window.title("솟수")
 
 en_input_number = tk.Entry(window)
-lab_print_prime_num = tk.Label(window,text="소수 판정 프로그램",border=2.4)
-btn_prime_num = tk.Button(window,text="소수 판정",background="orange",border=2.4,command=prime)
+lab_print_prime_and_odd_even_num = tk.Label(window,text="소수 판정 프로그램",border=2.4)
+btn_prime_num = tk.Button(window,text="소수 판정",background="orange",border=2.4,command=primeAndOddEven)
 
 
 
 #placement
-lab_print_prime_num.pack()
+lab_print_prime_and_odd_even_num.pack()
 en_input_number.pack(fill='x')
 btn_prime_num.pack(fill='x')
 
