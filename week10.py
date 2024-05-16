8f0ec398e1fa2ca5355bac0ea722f961943b6bfb
1di0t <jeonjerry0709@gmail.com>
2024-05-16 10:37:47 +0900

    version.0.0.3

--- week10.py
+++ week10.py
@@ -2,8 +2,18 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry("500x600")
-window.title("키오스크")
+window.geometry("500x200")#Size
+window.title("솟수")
+
+en_input_number = tk.Entry(window)
+lab_print_prime_num = tk.Label(window,text="소수 판정 프로그램",border=2.4)
+btn_prime_num = tk.Button(window,text="소수 판정",background="orange",border=2.4)
+
+#placement
+lab_print_prime_num.pack()
+en_input_number.pack(fill='x')
+btn_prime_num.pack(fill='x')
+
 
 
 window.mainloop()
