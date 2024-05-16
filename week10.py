799b5d22341e056074e3b1bfa1ebc986c97b7429
1di0t <jeonjerry0709@gmail.com>
2024-05-16 12:20:56 +0900

    version.0.0.1 grid version

--- week10.py
+++ week10.py
@@ -1,52 +1,31 @@
-#from tkinter import * #GUI module
 import tkinter as tk
 
-def is_prime(k) -> bool:
-    """
-    :param k:소수인지 판별할 양의 수
-    :return: If n is Prime :true, Not Prime : False
-    """
-    if k == 2:return True
-    elif k <= 1 or k % 2 == 0:return False
-    else:
-        i = 2
-        while i * i <= k:
-            if k % i == 0:
-                return False
-            i = i + 1
-        return True
+w = tk.Tk()
+w.geometry("200x300")
+w.title("그리두")
 
-def printPrime():
-    number = int(en_input_number.get())
-    text =""
-    if is_prime(number): text = "소수"
-    else: text = "소수가 아님"
-    lab_print_prime_and_odd_even_num.config(text=f"{number}는 {text}")
+btn_00 = tk.Button(w,text="00")
+btn_01 = tk.Button(w,text="01")
+btn_02 = tk.Button(w,text="02")
 
-def printOddEven():
-    number = int(en_input_number.get())
-    iseven = ""
-    if number%2==0: iseven = "짝수"
-    else : iseven = "홀수"
-    lab_print_prime_and_odd_even_num.config(text=f"{number}는 {iseven}")
+btn_10 = tk.Button(w,text="10")
+btn_11 = tk.Button(w,text="11")
+btn_12 = tk.Button(w,text="12")
 
-window = tk.Tk()
-window.geometry("500x400")#Size
-window.title("솟수")
+btn_20 = tk.Button(w,text="20")
+btn_21 = tk.Button(w,text="21")
+btn_22 = tk.Button(w,text="22")
 
-en_input_number = tk.Entry(window)
-lab_print_prime_and_odd_even_num = tk.Label(window,text="소수 판정 프로그램\t홀짝도 해줘용",border=2.4)
-btn_prime_num = tk.Button(window,height=10,text="소수 판정",background="orange",border=2.4,command=printPrime)#함수의 이름만 입력 괄호는 포함하지 않는다
-btn_odd_even_num = tk.Button(window,height=10,text="홀짝 판정",background="orange",border=2.4,command=printOddEven)
+btn_00.grid(row=0,column=0)
+btn_01.grid(row=0,column=1)
+btn_02.grid(row=0,column=2,rowspan=3,sticky="NS")#NorthSouth
 
+btn_10.grid(row=1,column=0)
+btn_11.grid(row=1,column=1)
+#btn_12.grid(row=1,column=2)
 
+btn_20.grid(row=2,column=0,columnspan=2,sticky="WE")
+#btn_21.grid(row=2,column=1)
+#btn_22.grid(row=2,column=2)
 
-#placement
-lab_print_prime_and_odd_even_num.pack()
-en_input_number.pack(fill='x')
-btn_prime_num.pack(fill='x')
-btn_odd_even_num.pack(fill='x')
-
-
-
-window.mainloop()
+w.mainloop()
