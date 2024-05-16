81921ebb3714e546b05400fd635bcea85c0bc45b
1di0t <jeonjerry0709@gmail.com>
2024-05-16 12:31:53 +0900

    version.0.2.0 grid ver prime

--- week10.py
+++ week10.py
@@ -21,32 +21,31 @@ def printPrime():
     text =""
     if is_prime(number): text = "소수"
     else: text = "소수가 아님"
-    lab_print_prime_and_odd_even_num.config(text=f"{number}는 {text}")
+    lab_print_prime_and_odd_even_num.config(text=f"\n{number}는 {text}\n")
 
 def printOddEven():
     number = int(en_input_number.get())
     iseven = ""
     if number%2==0: iseven = "짝수"
     else : iseven = "홀수"
-    lab_print_prime_and_odd_even_num.config(text=f"{number}는 {iseven}")
+    lab_print_prime_and_odd_even_num.config(text=f"\n{number}는 {iseven}\n")
 
 window = tk.Tk()
 window.geometry("500x400")#Size
 window.title("솟수")
 
 en_input_number = tk.Entry(window)
-lab_print_prime_and_odd_even_num = tk.Label(window,text="소수 판정 프로그램\t홀짝도 해줘용",border=2.4)
-btn_prime_num = tk.Button(window,height=10,text="소수 판정",background="orange",border=2.4,command=printPrime)#함수의 이름만 입력 괄호는 포함하지 않는다
-btn_odd_even_num = tk.Button(window,height=10,text="홀짝 판정",background="orange",border=2.4,command=printOddEven)
+lab_print_prime_and_odd_even_num = tk.Label(window,text="\n소수 판정 프로그램\n홀짝도 해줘용",border=2.4)
+btn_prime_num = tk.Button(window,text="소수 판정",background="orange",border=2.4,command=printPrime)#함수의 이름만 입력 괄호는 포함하지 않는다
+btn_odd_even_num = tk.Button(window,text="홀짝 판정",background="orange",border=2.4,command=printOddEven)
 
 
 
 #placement
-lab_print_prime_and_odd_even_num.pack()
-en_input_number.pack(fill='x')
-btn_prime_num.pack(fill='x')
-btn_odd_even_num.pack(fill='x')
-
+lab_print_prime_and_odd_even_num.grid(row=0,column=0,columnspan=2)
+en_input_number.grid(row=1,column=0,columnspan=2)
+btn_prime_num.grid(row=2,column=0,sticky="WE")
+btn_odd_even_num.grid(row=2,column=1,sticky="WE")
 
 
 window.mainloop()
