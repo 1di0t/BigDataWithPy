ee23c05842e874ac26f2f701e1acfdeea9727fb5
1di0t <jeonjerry0709@gmail.com>
2024-05-23 11:43:21 +0900

    Closur

--- week11.py
+++ week11.py
@@ -1,6 +1,8 @@
-def out_func(i,j):#3,2 to this parameter
-    def innerfunction(k,l):#get 3,2 in 4th line
-        return  k*l
-    return  innerfunction(i,j)#input 3,2
-
-print(out_func(3,2))
+def soldier(word):
+    def innerfunction():
+        return  f"{word} : 특공!"
+    return  innerfunction
+a = soldier("Sinper")
+b = soldier("Ranger")
+print(a())
+print(b())
