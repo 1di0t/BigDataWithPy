f60d229cc25cc3d37793754ff5dbbab486356883
1di0t <jeonjerry0709@gmail.com>
2024-05-23 11:24:16 +0900

    inner func

--- week11.py
+++ week11.py
@@ -1,4 +1,6 @@
-def test(**number):#dictionary
-    print(number)
+def out_func(i,j):#3,2 to this parameter
+    def innerfunction(k,l):#get 3,2 in 4th line
+        return  k*l
+    return  innerfunction(i,j)#input 3,2
 
-test(name='seong',lastname='il')
+print(out_func(3,2))
