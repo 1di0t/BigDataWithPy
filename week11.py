27c8361a70ebaa9d7b1960f5bfc4d80531dc72a6
1di0t <jeonjerry0709@gmail.com>
2024-05-23 12:19:28 +0900

    Lambda

--- week11.py
+++ week11.py
@@ -1,9 +1,6 @@
-def squares(num):
-    return num*num
-
 def square_numbers(numbers,func):
     for number in numbers:
         print(func(number))
 
 numbers = [i for i in range(1,11)]#list compreension
-square_numbers(numbers,squares)
+square_numbers(numbers,lambda number:number*number)
